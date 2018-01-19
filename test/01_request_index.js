2c48d4504ae9d76b146072fceca9588fb016056f
Marcus <marcus@proest.net>
2018-01-19 15:50:32 +0100

    Update 01_request_index.js

--- test/01_request_index.js
+++ test/01_request_index.js
@@ -1,9 +1,12 @@
+var expect  = require("chai").expect;
+var request = require("request");
+
 describe("Papierkorb.js WebApp", function() {
     describe("request /", function() {
     
         var url = "http://localhost:3000/";
         
-        it("returns status 200", function(done) {
+        it("should return status 200", function(done) {
             request(url, function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
                 done();
