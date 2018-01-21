5d548eb9b0c7e0b624205bdde93829d01904dcb1
Marcus Proest <marcus@proest.net>
2018-01-21 21:55:29 +0100

    new tests

--- test/01_prepare.js
+++ test/01_prepare.js
@@ -4,12 +4,22 @@ let conf = require('config').get('conf');
 let fs = require('fs');
 
 describe("Prepare Tests", function () {
-    describe("copy testfile /", function () {
-        let path = conf.doc.newpath + '/test.nld.pdf';
-        fs.copyFileSync('share/test.nld.pdf', path);
-        it("should exist in the new folder", function (done) {
+    describe("create new directory", function () {
+        let path = conf.doc.newpath;
+        fs.mkdir(path, function (err) {
+            //ignore if it already exists
+        });
+        it("should have created the new folder", function (done) {
             expect(fs.existsSync(path)).to.equal(true);
             done();
         });
-    })
+    }),
+        describe("copy testfile /", function () {
+            let path = conf.doc.newpath + 'test.nld.pdf';
+            fs.copyFileSync('share/test.nld.pdf', path);
+            it("should exist in the new folder", function (done) {
+                expect(fs.existsSync(path)).to.equal(true);
+                done();
+            });
+        })
 });
--- test/03_generate_request.js
+++ test/03_generate_request.js
@@ -1,7 +1,10 @@
 let expect = require("chai").expect;
 let request = require("request");
+let sleep = require('asleep');
+let mongo = require('mongodb').MongoClient;
+let conf = require('config').get('conf');
 
-describe("Basic Requests", function () {
+describe("Document Creation", function () {
     describe("request /new/test.nld.pdf/create/", function () {
 
         let url = "http://localhost:3000/new/test.nld.pdf/create/";
@@ -12,5 +15,63 @@ describe("Basic Requests", function () {
                 done();
             });
         });
-    })
+    }),
+        describe("wait for 15 seconds", function () {
+
+            it("should wait 15 seconds", function (done) {
+                sleep(15000).then(function () {
+                    expect(true).to.equal(true);
+                    done();
+                });
+            }).timeout(20000);
+        }),
+        describe("read document from database", function () {
+            it("should be in the database", function (done) {
+                let dburl = conf.db.constring + conf.db.db;
+                mongo.connect(dburl, function (err, db) {
+                    db.collection(conf.db.c_doc).findOne({}, function (err, result) {
+                        expect(result._id).to.not.be.undefined;
+                    });
+                });
+                done();
+            });
+        }),
+        describe("check the previews", function () {
+            it("there should be 5 previews", function (done) {
+                let dburl = conf.db.constring + conf.db.db;
+                mongo.connect(dburl, function (err, db) {
+                    db.collection(conf.db.c_doc).findOne({}, function (err, result) {
+                        expect(result.previews).to.equal(5);
+                    });
+                });
+                done();
+            });
+            it("should return status 200", function (done) {
+                let dburl = conf.db.constring + conf.db.db;
+                mongo.connect(dburl, function (err, db) {
+                    db.collection(conf.db.c_doc).findOne({}, function (err, result) {
+                        let url = "http://localhost:3000/doc/" + result._id + "/preview/0";
+                        request(url, function (error, response, body) {
+                            expect(response.statusCode).to.equal(200);
+                            done();
+                        });
+
+                    });
+                });
+                done();
+            });
+            it("should return content-type image/png", function (done) {
+                let dburl = conf.db.constring + conf.db.db;
+                mongo.connect(dburl, function (err, db) {
+                    db.collection(conf.db.c_doc).findOne({}, function (err, result) {
+                        let url = "http://localhost:3000/doc/" + result._id + "/preview/0";
+                        request(url, function (error, response, body) {
+                            expect(response.headers['content-type']).to.equal('image/png');
+                            done();
+                        });
+                    });
+                });
+                done();
+            });
+        });
 });
