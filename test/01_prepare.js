372cd5006e9b08f9049ca075623604b5c10f2c5a
Marcus Proest <marcus@proest.net>
2018-01-21 22:12:40 +0100

    small fixes

--- test/01_prepare.js
+++ test/01_prepare.js
@@ -16,7 +16,7 @@ describe("Prepare Tests", function () {
     }),
         describe("copy testfile /", function () {
             let path = conf.doc.newpath + 'test.nld.pdf';
-            fs.copyFileSync('share/test.nld.pdf', path);
+            fs.copyFileSync('./share/test.nld.pdf', path);
             it("should exist in the new folder", function (done) {
                 expect(fs.existsSync(path)).to.equal(true);
                 done();
