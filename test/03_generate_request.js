a5319e6838b460ea1e45428f630f10fe036714be
Marcus <marcus@proest.net>
2018-01-25 09:07:25 +0100

    Update 03_generate_request.js

--- test/03_generate_request.js
+++ test/03_generate_request.js
@@ -6,7 +6,7 @@ let conf = require('config').get('conf');
 
 describe("Document Creation", function () {
     describe("request /new/test.nld.pdf/create/", function () {
-
+        this.slow(0);
         let url = "http://localhost:3000/new/test.nld.pdf/create/";
 
         it("should return status 200", function (done) {
@@ -17,7 +17,7 @@ describe("Document Creation", function () {
         }).timeout(20000);
     }),
         describe("wait for 15 seconds", function () {
-
+            this.slow(20000);
             it("should wait 15 seconds", function (done) {
                 sleep(15000).then(function () {
                     expect(true).to.equal(true);
@@ -26,6 +26,7 @@ describe("Document Creation", function () {
             }).timeout(20000);
         }),
         describe("read document from database", function () {
+            this.slow(0);
             it("should be in the database", function (done) {
                 let dburl = conf.db.constring + conf.db.db;
                 mongo.connect(dburl, function (err, db) {
@@ -38,6 +39,7 @@ describe("Document Creation", function () {
             });
         }),
         describe("check the previews", function () {
+            this.slow(0);
             it("there should be 5 previews", function (done) {
                 let dburl = conf.db.constring + conf.db.db;
                 mongo.connect(dburl, function (err, db) {
