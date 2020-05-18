c6ce8c4ac0cdb5808352c47ed5c4554a3ecc095e
Marcus Proest <marcus@proest.net>
2020-05-18 09:11:00 +0200

    trying to get a global var....

--- test/040_api_requests.js
+++ test/040_api_requests.js
@@ -5,7 +5,7 @@ let conf = require("config").get("conf");
 let url = conf.test.schema + conf.test.host + ":80/api/v1/";
 
 describe("API requests", function () {
-  let sdoc = {};
+  global.sdoc = {};
 
   describe("request docs", function () {
     it("one document should have plausible metadata", function (done) {
@@ -17,20 +17,24 @@ describe("API requests", function () {
         expect(doc._id.match(rx)).to.not.be.null;
         expect(doc.previews).to.equal(5);
 
-        sdoc = doc;
+        global.sdoc = doc;
 
         done();
       });
     }).timeout(60000);
   }),
-    describe("request single doc " + sdoc._id, function () {
+    describe("request single doc " + global.sdoc._id, function () {
       it("single document should have same metadata as in /docs", function (done) {
-        request(url + "doc/" + sdoc._id, function (error, response, body) {
+        request(url + "doc/" + global.sdoc._id, function (
+          error,
+          response,
+          body
+        ) {
           let doc = JSON.parse(body)[0];
 
           expect(response.statusCode).to.equal(200);
-          expect(doc._id.to.equal(sdoc._id));
-          expect(doc.previews).to.equal(sdoc.previews);
+          expect(doc._id.to.equal(global.sdoc._id));
+          expect(doc.previews).to.equal(global.sdoc.previews);
 
           done();
         });
