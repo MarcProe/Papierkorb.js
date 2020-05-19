0126c36bb8f7bc84907a9e9e39ff4b4e25cabf46
Marcus Proest <marcus@proest.net>
2020-05-19 20:49:42 +0200

    add a test init command

---
+++ docker/init_test.js
@@ -0,0 +1,32 @@
+let conf = require("config").get("conf");
+let fse = require("fs-extra");
+const { exec } = require("child_process");
+
+process.env.NODE_ENV = "test";
+
+exec("docker-compose down", (err, stdout, stderr) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+
+  conf.test.dirs.forEach((dir) => {
+    if (dir.startsWith("./")) {
+      fse.removeSync(dir);
+      fse.ensureDirSync(dir);
+    } else {
+      throw new Error("Only relative Dir allowed");
+    }
+  });
+
+  exec("docker-compose up -d papierkorb-nginx", (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+});
